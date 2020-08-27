abf220cc462cbd9427b10637cefa9ce1f74ec0b3
iirfansardivand <iirfansardivand@gmail.com>
2020-08-27 12:02:51 +0430

    add client

---
+++ client/tester/src/components/CompositionComponent.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id" @click="increment">{{ todo.id }} - {{ todo.content }}</li>
+    </ul>
+    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
+    <p>Active: {{ active ? 'yes' : 'no' }}</p>
+    <p>Clicks on todos: {{ clickCount }}</p>
+    <button type="button" @click="mutateMutable">{{mutable}}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  PropType,
+  computed,
+  watch,
+  ref,
+  toRef,
+  Ref,
+  onMounted,
+} from "@vue/composition-api";
+import { Todo, Meta } from "./models";
+
+function useClickCount() {
+  const clickCount = ref(0);
+  function increment() {
+    clickCount.value += 1;
+    return clickCount.value;
+  }
+
+  return { clickCount, increment };
+}
+
+function useDisplayTodo(todos: Ref<Todo[]>) {
+  const todoCount = computed(() => todos.value.length);
+  return { todoCount };
+}
+
+function useCom() {
+  let mutable = ref(1);
+
+  const mutateMutable = function () {
+    mutable.value += 1;
+  };
+
+  return { mutable, mutateMutable };
+}
+
+export default defineComponent({
+  name: "CompositionComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    todos: {
+      type: (Array as unknown) as PropType<Todo[]>,
+      default: () => [],
+    },
+    meta: {
+      type: (Object as unknown) as PropType<Meta>,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+    },
+  },
+  setup(props) {
+    const myfunc = function () {
+      console.log(12);
+    };
+    const yourfunc = function () {
+      console.log(12);
+    };
+    onMounted(myfunc);
+    watch(useCom().mutable, (val: number) => console.log(val));
+
+    return {
+      ...useClickCount(),
+      ...useDisplayTodo(toRef(props, "todos")),
+      ...useCom(),
+    };
+  },
+});
+</script>
