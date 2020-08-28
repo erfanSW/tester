ab5824ba7bf190c4a464c588b9511906bea22fbc
iirfansardivand <iirfansardivand@gmail.com>
2020-08-29 02:28:13 +0430

    change database to postgres

--- server/src/models/users.model.ts
+++ server/src/models/users.model.ts
@@ -1,30 +1,64 @@
-// users-model.js - A mongoose model
-//
-// See http://mongoosejs.com/docs/models.html
+// See https://vincit.github.io/objection.js/#models
 // for more of what you can do here.
+import { Model } from 'objection';
+import Knex from 'knex';
 import { Application } from '../declarations';
 
-export default function (app: Application) {
-  const modelName = 'users';
-  const mongooseClient = app.get('mongooseClient');
-  const schema = new mongooseClient.Schema({
+class users extends Model {
+  createdAt!: string;
+  updatedAt!: string;
 
-    email: { type: String, unique: true, lowercase: true },
-    password: { type: String },
-
-
-    auth0Id: { type: String },
-
-    googleId: { type: String },
-
-  }, {
-    timestamps: true
-  });
-
-  // This is necessary to avoid model compilation errors in watch mode
-  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
-  if (mongooseClient.modelNames().includes(modelName)) {
-    mongooseClient.deleteModel(modelName);
+  static get tableName() {
+    return 'users';
   }
-  return mongooseClient.model(modelName, schema);
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['password'],
+
+      properties: {
+      
+        email: { type: ['string', 'null'] },
+        password: { type: 'string' },
+      
+        auth0Id: { type: 'string' },
+      
+      }
+    };
+  }
+
+  $beforeInsert() {
+    this.createdAt = this.updatedAt = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updatedAt = new Date().toISOString();
+  }
+}
+
+export default function (app: Application) {
+  const db: Knex = app.get('knex');
+
+  db.schema.hasTable('users').then(exists => {
+    if (!exists) {
+      db.schema.createTable('users', table => {
+        table.increments('id');
+      
+        table.string('email').unique();
+        table.string('password');
+      
+      
+        table.string('auth0Id');
+      
+        table.timestamp('createdAt');
+        table.timestamp('updatedAt');
+      })
+        .then(() => console.log('Created users table')) // eslint-disable-line no-console
+        .catch(e => console.error('Error creating users table', e)); // eslint-disable-line no-console
+    }
+  })
+    .catch(e => console.error('Error creating users table', e)); // eslint-disable-line no-console
+
+  return users;
 }
