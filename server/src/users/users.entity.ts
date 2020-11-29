ca00ee6f4c5fa315ea29201596cb5db96f25e9bf
iirfansardivand <iirfansardivand@gmail.com>
2020-11-29 12:56:25 +0330

    add username and password fields and role relation

--- server/src/users/users.entity.ts
+++ server/src/users/users.entity.ts
@@ -2,15 +2,21 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { Role } from '../roles/roles.entity';
 
 @Entity()
-export class User {
+export class Users {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   username: string;
 
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column()
+  password: string;
+
   @ManyToOne(type => Role, {
     onDelete: 'SET NULL',
   })
-  role: Role;
+  role: number;
 }
