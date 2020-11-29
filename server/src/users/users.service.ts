4c74ca9161d9802d30a181f96046aad688489e9d
iirfansardivand <iirfansardivand@gmail.com>
2020-11-29 12:51:12 +0330

    add create role and get all roles to service

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './users.entity';
+import { Users } from './users.entity';
+import { UserInterface } from './interface/users.interface';
 
 @Injectable()
 export class UsersService {
-  @InjectRepository(User)
-  private userRepository: Repository<User>;
+  @InjectRepository(Users)
+  private userRepository: Repository<Users>;
 
-  async findAll(): Promise<User[]> {
+  async findAll(): Promise<Users[]> {
     return this.userRepository.find();
   }
 
-  async createOne(): Promise<User> {
-    return this.userRepository.create();
+  async findOne(username): Promise<Users> {
+    return this.userRepository.findOne({
+      username,
+    });
+  }
+
+  async createOne(user: UserInterface): Promise<Users> {
+    return await this.userRepository.save(user);
   }
 }
