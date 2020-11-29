7340101bf78dd891b9311420d7d1843d307f1ac1
iirfansardivand <iirfansardivand@gmail.com>
2020-11-29 12:57:22 +0330

    update providers for user module

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { User } from './users.entity';
+import { Users } from './users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UserModule {}
